9dec6a3573b9b94d23c068d0c77b10ac58568f4f
maxuelong <maxuelong@baidu.com>
2021-06-03 09:40:26 +0800

    fix: add smelte class

---
+++ src/core/components/smelte/index.ts
@@ -0,0 +1,41 @@
+import Button from './button/button';
+import Checkbox from './checkbox/checkbox';
+import Switch from './switch/switch';
+import TextField from './text-field/text-field';
+import Select from './select/select';
+import Snackbar from './snackbar/snackbar';
+import List from './list/list';
+import RadioButtonGroup from './radio-button/radio-button-group';
+import Dialog from './dialog/dialog';
+import NavigationDrawer from './navigation-drawer/navigation-drawer'
+import ProgressCircular from './progress-circular/progress-circular';
+import ProgressLinear from './progress-linear/progress-linear';
+import Chip from './chip/chip';
+import Tabs from './tabs/tabs';
+import DataTable from './data-table/data-table';
+import Header from './data-table/header';
+import Tooltip from './tooltip/tooltip';
+import Treeview from './treeview/treeview';
+import DatePicker from './date-picker/date-picker';
+
+export {
+    Button,
+    Checkbox,
+    Switch,
+    TextField,
+    Select,
+    Snackbar,
+    List,
+    RadioButtonGroup,
+    Dialog,
+    NavigationDrawer,
+    ProgressCircular,
+    ProgressLinear,
+    Chip,
+    Tabs,
+    DataTable,
+    Header,
+    Tooltip,
+    Treeview,
+    DatePicker
+}
--- src/workbench/desktop/main.ts
+++ src/workbench/desktop/main.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import App from './app';
-import 'smelte/src/tailwind.css';
+import 'smelte/src/tailwind.css'
+import '../ui/css/global.css';
 
 const app = new App(document.body);
 app.bootstrap();
