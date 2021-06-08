dbd1cb8de5fbe5a0edadb6ee89d3ee884e4113c3
qiansc <van3008@163.com>
2021-06-08 17:39:21 +0800

    chore: modify global style & logo of storybook

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,5 +1,14 @@
-export default {
-  brandTitle: 'Cosmic Design Components',
+import { themes, create } from '@storybook/theming';
+
+const config = {
+  brandTitle: 'Cosmic Design Components Storybook',
   brandUrl: 'https://github.com/design-to-release/cosmic',
-  brandImage: 'https://raw.githubusercontent.com/design-to-release/cosmic/main/scripts/stories/assets/cosmic-logo.png',
+  brandImage: 'https://place-hold.it/350x150',
+};
+export default (mode) => {
+    return {
+        ...themes[mode],
+        ...config,
+        brandImage: `https://raw.githubusercontent.com/design-to-release/cosmic/main/scripts/stories/assets/cosmic-logo-${mode}.png`
+    }
 };
