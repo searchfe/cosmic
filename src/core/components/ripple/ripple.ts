7d2fe1b101bbe0d5ad890175ef64ad9f3ef103b9
qiansc <van3008@163.com>
2021-06-09 17:40:35 +0800

    chore: fix type

--- src/core/components/ripple/ripple.ts
+++ src/core/components/ripple/ripple.ts
@@ -1,40 +1,40 @@
-function ripple(color, centered) {
-  return function (event) {
+function ripple(color: string, centered: boolean) {
+  return function (event: any) {
     const target = event.currentTarget;
-    const circle = document.createElement("span");
+    const circle = document.createElement('span');
     const d = Math.max(target.clientWidth, target.clientHeight);
 
     const removeCircle = () => {
       circle.remove();
-      circle.removeEventListener("animationend", removeCircle);
+      circle.removeEventListener('animationend', removeCircle);
     };
 
-    circle.addEventListener("animationend", removeCircle);
+    circle.addEventListener('animationend', removeCircle);
     circle.style.width = circle.style.height = `${d}px`;
     const rect = target.getBoundingClientRect();
 
     if (centered) {
-      circle.classList.add("absolute", "top-0", "left-0", "ripple-centered", `bg-${color}-transDark`);
+      circle.classList.add('absolute', 'top-0', 'left-0', 'ripple-centered', `bg-${color}-transDark`);
     } else {
       circle.style.left = `${event.clientX - rect.left - d / 2}px`;
       circle.style.top = `${event.clientY - rect.top - d / 2}px`;
 
-      circle.classList.add("ripple-normal", `bg-${color}-trans`);
+      circle.classList.add('ripple-normal', `bg-${color}-trans`);
     }
 
-    circle.classList.add("ripple");
+    circle.classList.add('ripple');
 
     target.appendChild(circle);
   };
 }
 
-export default function r(color = "primary", centered = false) {
-  return function (node) {
+export default function r(color = 'primary', centered = false) {
+  return function (node: any) {
     const onMouseDown = ripple(color, centered);
-    node.addEventListener("mousedown", onMouseDown);
+    node.addEventListener('mousedown', onMouseDown);
 
     return {
-      onDestroy: () => node.removeEventListener("mousedown", onMouseDown),
+      onDestroy: () => node.removeEventListener('mousedown', onMouseDown),
     };
   };
 }
