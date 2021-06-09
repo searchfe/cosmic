01b4b24033361c8abd70f4f164a1da6155635f1f
biyingshuai <biyingshuai@gmail.com>
2021-06-09 17:32:47 +0800

    feat: core components of Selection Control and Dialog

---
+++ scripts/stories/selection-control/check-box.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+  import "smelte/src/tailwind.css";
+  import "../../../src/core/components/global.css";
+  import { Meta, Story, Template } from "@storybook/addon-svelte-csf";
+  import Checkbox from "../../../src/core/components/checkbox";
+
+</script>
+
+<Meta title="Selection Control" component="{Checkbox}" />
+
+<Template let:args>
+  <Checkbox label="A checkbox" />
+  <Checkbox color="secondary" label="A colored checkbox" />
+  <Checkbox disabled label="A disabled checkbox" />
+</Template>
+
+<Story name="CheckBox" />
