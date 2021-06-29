11bda00973c3bc8e76f2ca39d9808684fbd6397a
qiansc <van3008@163.com>
2021-06-29 14:58:01 +0800

    feat: fix offset when split

--- src/core/browser/layout/split-board.controller.ts
+++ src/core/browser/layout/split-board.controller.ts
@@ -12,7 +12,7 @@ export default class SplitBoardController {
     private activeId = -1;
 
     private expandActive = false;
-    private expandType: 0 | 1 | 2 = 0; // 是否超过偏移值 0 / hor | ver
+    private expandType = 0; // 是否超过偏移值 0 / hor | ver
 
     constructor(private view: SplitBoardView) {
         this.expandMoveHandler = (event: MouseEvent) => {
@@ -20,8 +20,8 @@ export default class SplitBoardController {
             this.checkExpandState(event.clientX, event.clientY);
             
         };
-        this.expandLeaveHandler = (event: MouseEvent) => { console.log('leave');  this.unactiveExpand(); };
-        this.expandMouseUpHandler = (event: MouseEvent) => { console.log('up'); this.unactiveExpand(); };
+        this.expandLeaveHandler = (event: MouseEvent) => { this.unactiveExpand(); };
+        this.expandMouseUpHandler = (event: MouseEvent) => { this.unactiveExpand(); };
         this.view.root.addEventListener('mousedown', (event: MouseEvent) => {
             this.onMouseDown(event);
         });
@@ -32,16 +32,18 @@ export default class SplitBoardController {
         this.view.items.forEach((item, i) => {
             if (target && item.root == target.parentElement && target.classList.contains('split-expand')) index = i;
         });
-
         if (index > -1) {
-            // 锚点mousedown
-            this.startX = event.clientX;
-            this.startY = event.clientY;
-            this.activeExpand();
-            this.activeId = index;
+            this.setActiveState(index, event.clientX, event.clientY);
         }
         event.preventDefault();
         event.stopPropagation();
+    }
+    public setActiveState(index: number, startX: number, startY: number ) {
+            // 锚点mousedown
+            this.startX = startX;
+            this.startY = startY;
+            this.activeExpand();
+            this.activeId = index;
     }
     private activeExpand() {
         this.view.root.addEventListener('mousemove', this.expandMoveHandler);
@@ -64,13 +66,21 @@ export default class SplitBoardController {
     private liveResize(clientX: number, clientY: number) {
         const item = this.view.items[this.activeId];
         if (this.expandType == 1 && this.view.direction == directionType.col) {
-            this.view.resizeAt(this.activeId, item.root.offsetLeft, item.root.clientWidth, clientX - 5); // -5px to matching cursor 
+            // horizontal expand column
+            this.view.resizeAt(this.activeId, item.root.getBoundingClientRect().left, item.root.clientWidth, clientX); // -5px to matching cursor 
+        // } else if (this.expandType == 1 && this.view.direction == directionType.row) {
+        //     // horizontal split row
+        //     console.log('horizontal split row', this.activeId, this.view);
+        // } else if (this.expandType == 2 && this.view.direction == directionType.col) {
+        //     // vertical split column
+        //     console.log('vertical split column', this.activeId, this.view);
         } else if (this.expandType == 2 && this.view.direction == directionType.row) {
-            this.view.resizeAt(this.activeId, item.root.offsetTop, item.root.clientHeight, clientY);
+            // vertical expand row
+            this.view.resizeAt(this.activeId, item.root.getBoundingClientRect().top, item.root.clientHeight, clientY);
         }
-        console.log(this.activeId, this.view);
+        
     }
-    private checkExpandState(clientX: number, clientY: number) {
+    public checkExpandState(clientX: number, clientY: number) {
         const offsetX = this.startX - clientX;
         const offsetY = this.startY - clientY;
         if ( offsetX > 0 && offsetY > 0 && (offsetX > 5 || offsetY > 5)) {
@@ -87,14 +97,20 @@ export default class SplitBoardController {
         if (offsetX > 30) {
             this.expandType = 1;
             this.view.setCursor('col-resize');
-            this.view.splitColumnAt(this.activeId, clientX);
+            this.view.splitColumnAt(this.activeId, clientX, clientY);
             return;
         }
         if (offsetY > 30) {
             this.expandType = 2;
             this.view.setCursor('row-resize');
-            this.view.splitRowAt(this.activeId, clientY);
+            this.view.splitRowAt(this.activeId, clientX, clientY);
+        }
+        if (
+            (this.expandType == 1 && this.view.direction == directionType.row) ||
+            (this.expandType == 2 && this.view.direction == directionType.col)
+            ) {
+                this.unactiveExpand();
         }
     }
-    
+     
 }
