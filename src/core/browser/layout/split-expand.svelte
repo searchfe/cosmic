3c490e0c551a745f0fc6e9b274225f5b5b31a250
qiansc <van3008@163.com>
2021-06-30 18:29:24 +0800

    feat: add merge action

--- src/core/browser/layout/split-expand.svelte
+++ src/core/browser/layout/split-expand.svelte
@@ -1,19 +1,70 @@
 <script>
-    export let show = true;
+    export let expandHidden = false;
+    export let mergeDirection = '';
 </script>
 
-<div class="split-expand" style="display:{show? '': 'none'}">
+<div class="split-expand" class:hidden="{expandHidden}">
+
+</div>
+
+<div class="split-merge"
+    class:merge-w="{mergeDirection=='merge-w'}"
+    class:merge-e="{mergeDirection=='merge-e'}"
+    class:merge-n="{mergeDirection=='merge-n'}"
+    class:merge-s="{mergeDirection=='merge-s'}"
+>
 
 </div>
 
 <style>
 .split-expand{
     position: absolute;
-    right:-3px;
-    bottom: 0px;
-    width: 10px;
-    height: 10px;
+    right: -7.5px;
+    bottom: -10px;
+    width: 15px;
+    height: 20px;
     cursor: crosshair;
     z-index: 9;
+}
+.split-merge{
+    display: none;
+    position: absolute;
+    width: 40%;
+    height: 40%;
+    background-repeat: no-repeat;
+    background-size: contain;
+    
+}
+.merge-n {
+    display: block;
+    left: 50%;
+    bottom: 0;
+    margin-left: -20%;
+    margin-top: -20%;
+    background-position: center bottom;
+}
+.merge-s {
+    display: block;
+    transform: rotate(180deg);
+    left: 50%;
+    top: 0;
+    margin-left: -20%;
+    background-position: center bottom;
+}
+.merge-w {
+    display: block;
+    transform: rotate(-90deg);
+    right: 0;
+    top: 50%;
+    margin-top: -20%;
+    background-position: center top;
+}
+.merge-e {
+    display: block;
+    transform: rotate(90deg);
+    left: 0;
+    top: 50%;
+    margin-top: -20%;
+    background-position: center top;
 }
 </style>
