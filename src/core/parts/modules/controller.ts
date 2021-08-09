f25a9e06f918fcfc5bb3f544a57e034f7fd4ca11
qiansc <van3008@163.com>
2021-08-09 18:53:45 +0800

    feat: add json module support

--- src/core/parts/modules/controller.ts
+++ src/core/parts/modules/controller.ts
@@ -1,4 +1,3 @@
-import type { SvelteComponentDev } from 'svelte/internal';
 export interface Controller {
   view: () => HTMLElement;
   init: () => Controller;
@@ -20,38 +19,4 @@ interface IComponentOptions {
   hydrate?: boolean;
   intro?: boolean;
   $$inline?: boolean;
-}
-
-export abstract class SVController implements Controller {
-  private comp: SvelteComponentDev;
-  private _view: HTMLElement;
-
-  public view() {
-    return this._view;
-  }
-
-  public init() {
-    this._view = this._view || document.createElement('div');
-    this.viewWillAppear();
-    this.comp = this.initWithComponent(this._view);
-    this.viewDidAppear();
-    return this;
-  }
-
-  abstract initWithComponent(target: Element): SvelteComponentDev;
-
-  abstract viewWillAppear(): void;
-  abstract viewDidAppear(): void;
-
-  abstract viewWillDisappear(): void;
-  abstract viewDidDisappear(): void;
-
-  public destroy() {
-    this.viewWillDisappear();
-    this.comp.$destroy();
-    if (this._view.parentNode) {
-      this._view.parentNode.removeChild(this._view);
-    }
-    this.viewDidDisappear();
-  }
 }
