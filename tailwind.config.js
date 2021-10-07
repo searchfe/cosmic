6a06bbf15cf20bc4f934a5ae5dd2eced394d62a3
biyingshuai <biyingshuai@gmail.com>
2021-10-07 23:23:33 +0800

    feat: add config for resource explorer

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,6 +76,9 @@ module.exports = {
     // maxWidth: widthConfigs,
     // minWidth: widthConfigs,
     fontSize: {
+      'h1': ['2.4rem', '2.4rem'],
+      'h2': ['2rem', '2rem'],
+      'h3': ['1.6rem', '1.6rem'],
       '3xs': ['0.9rem', '0.9rem'],
       '2xs': ['1rem', '1rem'],
       xs: ['1.1rem', '1.1rem'], // 标准字号
@@ -107,6 +110,9 @@ module.exports = {
         800: colors.trueGray[800],
         900: colors.trueGray[900],
       },
+      cblack: {
+        text: '#1f1f1f',
+      },
       error: {
         light: colors.red[400],
         DEFAULT: colors.red[400],
@@ -127,6 +133,18 @@ module.exports = {
       },
     },
     transitionDuration: { 100: '0ms', 200: '0ms' },
+    boxShadow: {
+      card: '1px 4px 6px 0 rgba(0, 0, 0, 0.1)',
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
+      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+     '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+      none: 'none',
+    },
   },
   // Extend Tailwind theme
   variants: {
