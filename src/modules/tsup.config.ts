ff4db69e31533d70875a26ba902fc41d30d8e6c9
木杉 <zhmushan@qq.com>
2021-12-14 15:57:49 +0800

    rollup -> tsup

--- src/modules/tsup.config.ts
+++ src/modules/tsup.config.ts
@@ -1,17 +1,14 @@
-import type { Options } from 'tsup'
+import GlobalConfig from '../../tsup.config';
+import { defineConfig } from 'tsup';
 
-const config: Options = {
-  format: ['esm'],
-  entryPoints: [
-    // 'draw-canvas/skia-wasm/index.ts',
-    // 'menu-group/index.ts',
-    // 'property-panel',
-    // 'resource-explorer',
-    // 'shape-canvas',
-    // 'workbench/index.ts'
+export default defineConfig({
+  ...GlobalConfig,
+  entry: [
+    'draw-canvas/skia-wasm/index.ts',
+    'menu-group/index.ts',
+    'property-panel/index.ts',
+    'resource-explorer/index.ts',
+    'shape-canvas/index.ts',
+    'workbench/index.ts',
   ],
-  target: 'es2019',
-  sourcemap: 'external'
-}
-
-export default config
+});
