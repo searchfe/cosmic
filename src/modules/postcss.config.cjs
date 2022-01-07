d4fd6223a818f8134258130514cfd45f842784ef
木杉 <zhmushan@qq.com>
2022-01-07 17:35:21 +0800

    Support CSS module (#72)

--- src/modules/postcss.config.cjs
+++ src/modules/postcss.config.cjs
@@ -1,3 +1,15 @@
 module.exports = {
-    plugins: [require('tailwindcss'), require('autoprefixer')],
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('postcss-modules')({
+            getJSON() {},
+            generateScopedName(name, filename, css) {
+                if (filename.endsWith('.module.css')) {
+                    return require('postcss-modules/build/generateScopedName').default(name, filename, css);
+                }
+                return name;
+            },
+        }),
+    ],
 };
