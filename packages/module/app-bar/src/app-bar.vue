e7f65864c92cadd63b581357c792423f17f91317
Qian.Sicheng <van3008@163.com>
2022-03-20 23:58:18 +0800

    feat: update style of menu (#118)

--- packages/module/app-bar/src/app-bar.vue
+++ packages/module/app-bar/src/app-bar.vue
@@ -4,14 +4,13 @@ import WorkbenchList from './workbench-list.vue';
 </script>
 
 <template>
-    <div :class="[$style.root, 'flex justify-between items-center']">
+    <div :class="[$style.root, 'h-26 flex justify-between items-center cos-mode-reverse']">
         <menu-list class="mr-40" />
         <workbench-list />
     </div>
 </template>
 <style module>
 .root {
-    background-color: var(--color-gray-900);
-    height: 2.6rem;
+    background-color: var(--color-gray-200);
 }
 </style>
--- packages/module/app-bar/src/menu-list.vue
+++ packages/module/app-bar/src/menu-list.vue
@@ -1,16 +1,15 @@
 <script lang="ts" setup>
 import { Button } from 'cosmic-vue';
-import ButtonStyle from './button.module.css';
-import ButtonRoundedStyle from './button-rounded.module.css';
+import buttonMenu from './button-menu.module.css';
 </script>
 <template>
     <div class="flex justify-start ml-6">
-        <Button :styles="ButtonRoundedStyle" size="xs">
+        <Button class="square" :styles="buttonMenu" size="xs">
             <i-cosmic-play />
         </Button>
-        <Button :styles="ButtonStyle" size="xs">文件</Button>
-        <Button :styles="ButtonStyle" size="xs">编辑</Button>
-        <Button :styles="ButtonStyle" size="xs">帮助</Button>
+        <Button class="min-w-40 mx-1" :styles="buttonMenu" size="xs">文件</Button>
+        <Button class="min-w-40 mx-1" :styles="buttonMenu" size="xs">编辑</Button>
+        <Button class="min-w-40 mx-1" :styles="buttonMenu" size="xs">帮助</Button>
         <!-- <Button>文件</Button>
         <Button>编辑</Button>
         <Button>帮助</Button> -->
