16d0dc40f6c3fcb011aa7842ec05fbe329421248
Qian.Sicheng <van3008@163.com>
2022-03-27 16:22:04 +0800

    feat: add service for node (#142)

--- packages/core/parts/node/index.ts
+++ packages/core/parts/node/index.ts
@@ -2,7 +2,8 @@ import DocumentNode from './base/document-node';
 import PageNode from './base/page-node';
 import ComponentNode from './sence/component-node';
 import TextNode from './sence/text-node';
+import { type SenceNode } from './sence/sence-node';
 
 export { DocumentNode, PageNode };
 
-export { ComponentNode, TextNode};
+export { SenceNode, ComponentNode, TextNode};
