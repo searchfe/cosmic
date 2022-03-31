98abcc4c52c854f7024a82fa762fd1028aeaa446
qiansc <van3008@163.com>
2022-03-31 14:09:56 +0800

    feat: add creator

---
+++ packages/module/app-editor/src/common/creator.ts
@@ -0,0 +1,68 @@
+import { service } from '@cosmic/core/browser';
+import { FrameNode, GroupNode, PageNode, SceneNode } from '@cosmic/core/parts';
+import { type ObjectDirective } from 'vue';
+
+export default {
+    mounted(el: HTMLElement, binding) {
+        const container = binding.instance?.$.appContext.provides?.container;
+        const targetNode: PageNode | FrameNode | GroupNode = binding.value.target;
+        const root: HTMLElement = binding.value.container && el.getElementsByClassName(binding.value.container)[0] || el;
+        if (!container) return;
+        const toolService: service.ToolService = container.get(service.TOKENS.Tool);
+        const nodeService: service.NodeService = container.get(service.TOKENS.Node);
+
+        let startX: number, startY: number, originX: number, originY: number;
+        let editState: service.ToolState | undefined;
+        let editingChild: SceneNode | undefined;
+        toolService.state().subscribe((state: service.ToolState) => {
+            if (
+                state === service.ToolState.Frame
+            ) {
+                editState = service.ToolState.Frame;
+            } else {
+                editState = undefined;
+            }
+        });
+        el.addEventListener('mousedown', (event: MouseEvent) => {
+            if(!editState) return;
+            event.preventDefault();
+            event.stopPropagation();
+            const style = window.getComputedStyle(root);
+            const matrix = new DOMMatrixReadOnly(style.transform);
+            startX = event.offsetX;
+            startY = event.offsetY;
+            originX = event.offsetX - matrix.m41;
+            originY = event.offsetY - matrix.m42;
+            appendChild(originX, originY);
+        });
+        el.addEventListener('mousemove', (event: MouseEvent) => {
+            if (editState && editingChild) {
+                const width = Math.max(event.offsetX - startX, 10);
+                const height = Math.max(event.offsetY - startY, 10);
+                editingChild.remove();
+                appendChild(originX, originY, width, height);
+            }
+        });
+        el.addEventListener('mouseup', () => {
+            if(editState) {
+                toolService.cancel(editState);
+                editState = undefined;
+            }
+            if (editingChild) editingChild = undefined;
+            startX = startY = originX = originY = 0;
+        });
+
+        function appendChild(x: number, y: number, width = 10, height = 10) {
+            switch(editState) {
+                case service.ToolState.Frame:
+                    editingChild = nodeService.addFrame(targetNode, {
+                        x,
+                        y,
+                        width,
+                        height,
+                    });
+                break;
+            }
+        }
+    },
+} as ObjectDirective;
---
+++ packages/module/app-editor/src/render/frame.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { type FrameNode, util } from '@cosmic/core/parts';
+import { onMounted, ref } from 'vue';
+import Wrapper from '../common/wrapper.vue';
+import { service } from '@cosmic/core/browser';
+import { inject } from '@cosmic/core/parts';
+
+interface FrameProps {
+    node: FrameNode,
+}
+
+const props = withDefaults(defineProps<FrameProps>(), {
+
+});
+onMounted(() => {
+    // console.log(props.node, props.node.height, props.node.x);
+});
+
+const nodeService = inject<service.NodeService>(service.TOKENS.Node);
+const selected = ref(true);
+
+nodeService.selection.subscribe(nodes => {
+    if(nodes.filter(node => node.id == props.node.id).length) {
+        selected.value = true;
+    } else {
+        selected.value = false;
+    }
+});
+
+</script>
+<template>
+    <div
+        v-creator="{target: node}"
+        class="frame-render"
+        :style="{
+            position: 'absolute', // 需要根据模式切换
+            top: node.y + 'px',
+            left: node.x + 'px',
+            width: node.width + 'px',
+            height: node.height + 'px',
+            background: util.toBackgroundStyle(node?.backgrounds[0]),
+        }"
+    >
+        <div class="relative">
+            <children-render :children="node.children" />
+        </div>
+        <wrapper :hidden="!selected" :info="node.width + '×' + node.height" />
+    </div>
+</template>
