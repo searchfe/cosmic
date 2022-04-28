3a7e435d416f3185e43f7c933ece38e4625ae451
Qian.Sicheng <van3008@163.com>
2022-04-29 03:55:41 +0800

    feat: update layout (#272)

--- packages/core/parts/internals/document/mixin/layout-mixin.ts
+++ packages/core/parts/internals/document/mixin/layout-mixin.ts
@@ -1,15 +1,12 @@
 type Transform = Internal.Transform;
 type Rect = Internal.Rect;
+import ConstraintMixin, { Constraints, ConstraintType } from './constraint-mixin';
+import { round } from '@cosmic/core/lodash';
 export default class LayoutMixin implements Internal.LayoutMixin {
     readonly absoluteTransform: Transform;
     relativeTransform: Transform;
     x = 0;
     y = 0;
-
-    VerticalStretch = 0;
-    HorizontalStretch = 0;
-    b = 0;
-    r = 0;
 
     rotation: number; // In degrees
 
@@ -23,39 +20,15 @@ export default class LayoutMixin implements Internal.LayoutMixin {
 
 
     resize(width: number, height: number) {
-        // switch(this.HorizontalLayout) {
-        //     case 0:
-        //         this.width = width;
-        //         break;
-        //     case 1:
-        //         this.x = ((this as any).parent.width - this.width - this.r);
-        //         this.width = width;
-        //         break;
-        //     case 2:
-        //         this.width = ((this as any).parent.width - this.x - this.r);
-        //         break;
-        //     case 3:
-        //         break;
-        //     case 4:
-        //         break;
-        // }
-        // switch(this.VerticalLayout) {
-        //     case 0:
-        //         this.height = height;
-        //         break;
-        //     case 1:
-        //         this.y = ((this as any).parent.height - this.height - this.b);
-        //         this.height = height;
-        //         break;
-        //     case 2:
-        //         this.height = ((this as any).parent.height - this.y - this.b);
-        //         break;
-        //     case 3:
-        //         break;
-        //     case 4:
-        //         break;
-        // }
+        const deltaX = width - this.width;
+        const deltaY = height - this.height;
+
+        this.width = width;
+        this.height = height;
         ((this as any).children || []).forEach((child: any) => {
+            if(child.constraints && child.layoutAlign) {
+                layoutChild(this as any, child, deltaX, deltaY);
+            }
             child.resize(child.width, child.height);
             child.update();
         });
@@ -65,5 +38,38 @@ export default class LayoutMixin implements Internal.LayoutMixin {
     }
     rescale(scale: number) {
         // TODO
+    }
+}
+function layoutChild(parent: Internal.BaseFrameMixin,node: LayoutMixin & ConstraintMixin, deltaX: number, deltaY: number) {
+    if (parent.layoutMode !== 'NONE') return;
+    switch(node.constraints.horizontal) {
+        case ConstraintType.MAX:
+            node.x = round(node.x + deltaX);
+            break;
+        case ConstraintType.STRETCH:
+            node.width = round(node.width + deltaX);
+            break;
+        case ConstraintType.CENTER:
+            node.x = node.x + deltaX / 2;
+            break;
+        case ConstraintType.SCALE:
+            node.width = round(node.width / (parent.width - deltaX) * parent.width, 1);
+            node.x = round(node.x / (parent.width - deltaX) * parent.width, 1);
+            break;
+    }
+    switch(node.constraints.vertical) {
+        case ConstraintType.MAX:
+            node.y = round(node.y + deltaY);
+            break;
+        case ConstraintType.STRETCH:
+            node.height = round(node.height + deltaY);
+            break;
+        case ConstraintType.CENTER:
+            node.y = node.y + deltaY / 2;
+            break;
+        case ConstraintType.SCALE:
+            node.height = round(node.height / (parent.height - deltaY) * parent.height, 1);
+            node.y = round(node.y / (parent.height - deltaY) * parent.height, 1);
+            break;
     }
 }
