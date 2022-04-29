3b1c72d22c0546e528e10f1fedf13a42ace8c714
qiansc <van3008@163.com>
2022-04-29 20:48:47 +0800

    feat: add-flex

---
+++ packages/module/widget-frame-base/src/common/layout-mode.ts
@@ -0,0 +1,11 @@
+export const layoutModeLabel: {[index: string]: string} = {
+    'absolute': '自由布局',
+    'flex' : '自适应布局',
+    'grid' : '栅格布局',
+};
+// export enum layoutModeValue {
+//     'absolute '= '自由布局',
+//     'flex' = '自适应布局',
+//     'grid' = '栅格布局',
+// }
+export const layoutModeOptions = ['absolute', 'flex', 'grid'];
