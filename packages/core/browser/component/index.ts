540c9deb0e271fcfaf189b1f5a2a35114f1aefbd
下雪 <dl880509@163.com>
2022-05-03 22:13:12 +0800

    fix: 🐛 update cosmic-vue version (#280)

---
+++ packages/core/browser/component/index.ts
@@ -0,0 +1,9 @@
+import MColor from './color/color.vue';
+import MClolorWidget from './color/color-widget.vue';
+import MTitle from './title/title.vue';
+import MWidget from './widget/widget.vue';
+import MStandardModal from './modal/standard-modal.vue';
+import MDetailModal from './modal/detail-modal.vue';
+import MStandard from './standard/standard.vue';
+
+export { MColor, MTitle, MWidget, MStandardModal, MStandard, MDetailModal, MClolorWidget};
