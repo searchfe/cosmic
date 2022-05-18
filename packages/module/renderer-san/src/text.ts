934a101e4049986aeb78bf4d08aa01e149f77685
Qian.Sicheng <van3008@163.com>
2022-05-18 19:40:06 +0800

    fix: add bca-disable (#297)

--- packages/module/renderer-san/src/text.ts
+++ packages/module/renderer-san/src/text.ts
@@ -1,6 +1,7 @@
 import { Component } from 'san';
 
 export class Text extends Component {
+    /* bca-disable */ 
     static template =  `
         <div style="{{style}}" class="{{class}}">
            {{ value | raw }}
